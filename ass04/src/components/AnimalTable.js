d905a6e7ba2a6204ce35c5cfb4c3943b44973164
prathibhani <139643234+prathibhani@users.noreply.github.com>
2024-11-24 02:07:17 +0000

    checks the selected animal is matching to the random animal in the array

--- ass04/src/components/AnimalTable.js
+++ ass04/src/components/AnimalTable.js
@@ -15,5 +15,13 @@ const generateRandomAnimal =()=>{
     setRandomAnimal(animals[randomIndex]);
     setResult('');
 }
+const handleAnimalClick=(selectedAnimal)=>{
+  if(selectedAnimal === randomAnimal.name){
+    setResult('WIN');
+  }  else{
+    setResult('LOSE');
+  }
+
+}
 
 }
