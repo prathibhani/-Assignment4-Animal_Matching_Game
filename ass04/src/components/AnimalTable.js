962dcf072be0a4bd0474a98994062f9c0a662a24
prathibhani <139643234+prathibhani@users.noreply.github.com>
2024-11-24 02:03:43 +0000

    generate a random animal from the array

--- ass04/src/components/AnimalTable.js
+++ ass04/src/components/AnimalTable.js
@@ -1,8 +1,19 @@
-import React,{useState} from 'react';
+import React,{useEffect,useState} from 'react';
 import {animals} from '../data/AnimalsDb';
 import '../assests/css/compo.css';
 
 function AnimalTable(props){
     const [randomAnimal,setRandomAimal]=useState(null);
     const[result,setResult]=useState('');
+
+useEffect(()=>{
+    generateRandomAnimal();
+},[]);
+
+const generateRandomAnimal =()=>{
+   const randomIndex=Math.floor(Math.random()*10)+1;
+    setRandomAnimal(animals[randomIndex]);
+    setResult('');
+}
+
 }
